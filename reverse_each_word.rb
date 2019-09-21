2e4713eb9558bf476083330b0b866bbe180d07a9
Samantha Phipps <sammi.phipps@gmail.com>
2019-09-21 03:05:20 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,7 @@
+def reverse_each_word(sentence)
+  reversed_array = []
+  sentence_array = sentence.split(" ")
+  sentence_array.each{|word| reversed_array << word.reverse}
+  reversed_array.join(" ")
+end 
+
