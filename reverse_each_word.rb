0b05242421b1ce963ad9d350aaa00174cae89985
Samantha Phipps <sammi.phipps@gmail.com>
2019-09-21 03:22:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(sentence)
   reversed_array = []
+  reversed_array_collect = []
   sentence_array = sentence.split(" ")
   sentence_array.each{|word| reversed_array << word.reverse}
+  sentence_array.collect{|word| reversed_array_collect << word.reverse}
   reversed_array.join(" ")
-  
 end 
