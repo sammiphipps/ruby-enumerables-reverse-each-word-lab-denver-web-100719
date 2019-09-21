5f7348f999c7e862440657ab09cb76f37ae3fdbc
Samantha Phipps <sammi.phipps@gmail.com>
2019-09-21 03:20:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,4 +5,4 @@ def reverse_each_word(sentence)
   reversed_array.join(" ")
 end 
 
-["Verifying that collect is being called"].collect{}
+["Verifying that collect is being called"].collect{|sentence| reverse_each_word(sentence)}
