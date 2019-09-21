a1eff60f2f8eab6a1af47c8e1c891fabd63fd229
Samantha Phipps <sammi.phipps@gmail.com>
2019-09-21 03:21:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(sentence)
   reversed_array = []
   sentence_array = sentence.split(" ")
-  sentence_array.each{|word| reversed_array << word.reverse}
+  z
   reversed_array.join(" ")
+  
 end 
-
-["Verifying that collect is being called"].collect{|sentence| reverse_each_word(sentence)}
